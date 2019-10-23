56622e3fd79e29543775d8513ceae0736a10d42c
AlexSaxena <saxenaalex@gmail.com>
2019-10-23 12:11:13 +0200

    Adds gems rspec-rails,shoulda-matchers,factory_bot_rails, Adds Shoulda matchers configurations,Adds format and color to .rspec file,Configures generators,Cleans out rails-helper.rb.

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,15 @@ Bundler.require(*Rails.groups)
 
 module FakeNewsApi
   class Application < Rails::Application
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+      generate.helper_specs false
+      generate.routing_specs false
+      generate.controller_specs false
+    end
+    
     config.load_defaults 6.0
     config.api_only = true
   end
