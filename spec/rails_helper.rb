d4170db899d6da00b6e493bea3fa6388aa0fd12e
AlexSaxena <saxenaalex@gmail.com>
2019-10-23 15:24:45 +0200

    Removes rack-cors gem, Removes Support folder with factorybot and shoulda-matchers files, Removes Support folder with factorybot and shoulda-matchers files.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,6 +21,8 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include FactoryBot::Syntax::Methods
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
 end
 
 Shoulda::Matchers.configure do |config|
