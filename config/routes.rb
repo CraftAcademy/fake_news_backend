808fad31f64e21bbad1305a71ea38c71f3f2f04d
AlexSaxena <saxenaalex@gmail.com>
2019-10-24 11:36:19 +0200

    Adds unit tests for article, Configures validations.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-    namespace :v1, defaults: { format: :json } do 
-      resources :article, only: [:index]
+  namespace :v1, defaults: { format: :json } do 
+    resources :article, only: [:index]
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  describe 'DB table' do
+    it { is_expected.to have_db_column :id }
+    it { is_expected.to have_db_column :title }
+    it { is_expected.to have_db_column :content }
+    it { is_expected.to have_db_column :author }
+  end
+
+  describe 'Validation' do
+    it { is_expected.to validate_presence_of :title }
+    it { is_expected.to validate_presence_of :content }
+    it { is_expected.to validate_presence_of :author }
+  end
+
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(FactoryBot.create(:article)).to be_valid
+    end
+  end
+end  
