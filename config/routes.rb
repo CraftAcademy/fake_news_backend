e365d9cbc9d990d88e6b97a445c672cb6f1f2d1e
AlexSaxena <saxenaalex@gmail.com>
2019-10-25 14:52:41 +0200

    Adds routes for devise_token_auth, Adds request specs for user registration.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   namespace :v1, defaults: { format: :json } do 
     resources :articles, only: [:index]
+    mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
   end
 end
