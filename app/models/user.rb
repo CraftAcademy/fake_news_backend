470d68be17e8cf9179ed5d0a7f34ffe6668a41f9
AlexSaxena <saxenaalex@gmail.com>
2019-10-26 14:15:24 +0200

    Cleans out comments and removes duplicate code line.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
-# app/models/user.rb
 class User < ActiveRecord::Base
   extend Devise::Models
-    # Include default devise modules.
     devise :database_authenticatable, :registerable,
             :recoverable, :rememberable, :trackable, :validatable
     include DeviseTokenAuth::Concerns::User
