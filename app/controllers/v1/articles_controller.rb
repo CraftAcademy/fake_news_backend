39f33db2fb0e4b9211ff998674e6efa38199cee2
clalil <cliljander@gmail.com>
2019-10-28 17:53:09 +0100

    Configure routes, updates articles controller with create function for image attachment to articles.

--- app/controllers/v1/articles_controller.rb
+++ app/controllers/v1/articles_controller.rb
@@ -8,7 +8,27 @@ class V1::ArticlesController < ApplicationController
     end
   end
 
+  def create
+    @article = Article.create(article_params)
+    attach_image
+    if @article.persisted? && @article.image.attached?
+      render json: { message: 'Article was successfully created' }
+    else
+      render_error_message(@article.errors.first.to_sentence, 400)
+    end
+  end
+
   private
+
+  def article_params
+    params.permit([:title, :content, keys: [:image]])
+  end
+
+  def attach_image
+    if params['image'] && params['image'].present?
+      DecodeService.attach_image(params['image'].first, @article.image)
+    end
+  end
   
   def render_error_message(message, status) 
     render json: { error_message: message }, status: status
