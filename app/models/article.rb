48933ef4af6f175a6e767e18d92ba3254d58e7c2
clalil <cliljander@gmail.com>
2019-10-29 13:54:19 +0100

    Remove duplicate validations for article model.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,5 @@
 class Article < ApplicationRecord
-  validates_presence_of :title, :content
-  validates :title, length: { minimum: 3, maximum: 50 }
-  validates :content, length: { minimum: 10, maximum: 250 }
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :content, presence: true, length: { minimum: 10, maximum: 250 }
   has_one_attached :image
 end
