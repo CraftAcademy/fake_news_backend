9e0ab48313b6b63b3671a1a9930fffbcd5f23595
clalil <cliljander@gmail.com>
2019-10-29 13:23:59 +0100

    Completes sad path for create article.

--- spec/requests/v1/api_provides_possibility_for_journalist_to_create_article_spec.rb
+++ spec/requests/v1/api_provides_possibility_for_journalist_to_create_article_spec.rb
@@ -27,13 +27,13 @@ RSpec.describe 'Can create article with attributes' do
     end
   end
 
-  describe "cannot post article successfully" do
+  describe "cannot post article successfully with incomplete title" do
     let(:headers) { {HTTP_ACCEPT:"application/json"} }
 
     before do
       post '/v1/articles', params: {
         title: "Wh",
-        content: "Oh",
+        content: "Oh my apple pie how everything is awesome!",
         image: {
           type: 'application/jpg',
           encoder: 'name=new_iphone.jpg;base64',
@@ -53,13 +53,31 @@ RSpec.describe 'Can create article with attributes' do
       article = Article.find_by(title: response.request.params['title'])
       expect(response_json['error_message']).to eq 'title and is too short (minimum is 3 characters)'
     end
+  end
 
-    it 'returns en error status when content is to short' do
-      article = Article.find_by(content: response.request.params['content'])
-      expect(response.status).to eq 400 
+  describe "cannot post article successfully with incomplete content" do
+    let(:headers) { {HTTP_ACCEPT:"application/json"} }
+
+    before do
+      post '/v1/articles', params: {
+        title: "Oh my apple pie how everything is awesome!",
+        content: "Wh",
+        image: {
+          type: 'application/jpg',
+          encoder: 'name=new_iphone.jpg;base64',
+          data: 'iVBORw0KGgoAAAANSUhEUgAABjAAAAOmCAYAAABFYNwHAAAgAElEQVR4XuzdB3gU1cLG8Te9EEgISQi9I71KFbBXbFixN6zfvSiIjSuKInoVFOyIDcWuiKiIol4Q6SBVOtI7IYSWBkm',
+          extension: 'jpg'
+        }
+      },
+      headers: headers
     end
 
-    it 'returns en error message when content is to short' do
+    it "returns an error status when title is incomplete" do
+      article = Article.find_by(content: response.request.params['content'])
+      expect(response.status).to eq 400
+    end
+
+    it "returns an error message when title is incomplete" do
       article = Article.find_by(content: response.request.params['content'])
       expect(response_json['error_message']).to eq 'content and is too short (minimum is 10 characters)'
     end
