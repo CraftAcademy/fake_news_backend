9132e89cfe678d8cf16bb5b51152c3370ccb1ed3
luca <l.lobacher@web.de>
2019-10-31 12:53:45 +0100

    Adds image function to Articles serializer

--- app/serializers/articles/index_serializer.rb
+++ app/serializers/articles/index_serializer.rb
@@ -1,3 +1,7 @@
 class Articles::IndexSerializer < ActiveModel::Serializer  
-  attributes :id, :title, :content
+  attributes :id, :title, :content, :image
+
+  def image
+    object.image.service_url(expires_in: 1.hour, disposition: 'inline')
+  end
 end
