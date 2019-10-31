fbe3891768235cf16529024fde7db798988cd5b8
jonas <jonas.masharqa@gmail.com>
2019-10-31 15:55:38 +0100

    Modifies test to accomodate attached images
    Configures FactoryBot to include attached image,
    Configures image method to accomodate test env,
    Sets default url option in test.rb
    Configures list of article spec to test title, content and image.

--- app/serializers/articles/index_serializer.rb
+++ app/serializers/articles/index_serializer.rb
@@ -1,7 +1,13 @@
-class Articles::IndexSerializer < ActiveModel::Serializer  
+class Articles::IndexSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
+    
   attributes :id, :title, :content, :image
 
   def image
-    object.image.service_url(expires_in: 1.hour, disposition: 'inline')
+    if Rails.env.test?
+      rails_blob_url(object.image)
+    else
+      object.image.service_url(expires_in: 1.hour, disposition: 'inline')
+    end
   end
 end
