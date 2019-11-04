ca04662f74b111105a6d4dc318218eaf46cb3d04
AlexSaxena <saxenaalex@gmail.com>
2019-11-04 20:17:28 +0100

    Renames payment controller to be plural, Configures routes for payment.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :v1, defaults: { format: :json } do 
     resources :articles, only: [:index, :create, :show, :update]
+    resources :payments, only: [:index, :create]
   end
   mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
 end
