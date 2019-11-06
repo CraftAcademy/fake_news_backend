922635088cb73ff59d3ec0e4d722ac9c9f9a28d3
AlexSaxena <saxenaalex@gmail.com>
2019-11-06 17:10:25 +0100

    Updates spec test with error status and message.

--- spec/requests/v1/api_provides_a_way_for_user_to_pay_spec.rb
+++ spec/requests/v1/api_provides_a_way_for_user_to_pay_spec.rb
@@ -9,15 +9,19 @@ RSpec.describe 'Provides a way to add subscription' do
     stripeToken: stripe_helper.generate_card_token    
     }, headers: headers
        user.reload }
-  let(:unsuccessful_token) { post '/v1/payments', params: {  stripeEmail: user.email,
+  let(:unsuccessful_token_1) { post '/v1/payments', params: {  stripeEmail: user.email,
     stripeToken: nil    
     }, headers: headers
        user.reload }
+  let(:unsuccessful_token_2) { post '/v1/payments', params: {  stripeEmail: user.email,
+    stripeToken: 5    
+    }, headers: headers
+        user.reload }
 
   before { StripeMock.start }
   after { StripeMock.stop }
-  
-  describe 'User would like to pay for subscription' do
+
+  describe 'Successful Payment' do
 
     it 'User successfully pays for subscription and gets a Subscriber role' do
       successful_token
@@ -33,20 +37,36 @@ RSpec.describe 'Provides a way to add subscription' do
       successful_token
       expect(response_json['message']).to eq 'Transaction successful'
     end
+  end
 
+  describe "Unsuccessful Payment w/o stripe token" do
+      
     it 'User role remains due to missing Stripe token' do
-      unsuccessful_token
+      unsuccessful_token_1
       expect(user.role).to eq('user')
     end
 
     it "Transaction recieves an error status of 402" do
-      unsuccessful_token   
+      unsuccessful_token_1 
       expect(response.status).to eq 402
     end
 
     it 'Response message indicates a unsuccessful transaction' do
-      unsuccessful_token
+      unsuccessful_token_1
       expect(response_json['errors']).to eq 'No stripe token detected'
+    end
+  end
+
+  describe "Unsuccessful Payment w. invalid token" do
+    
+    it "Returns error message" do
+      unsuccessful_token_2
+      expect(response_json['errors']).to eq 'Invalid token id: 5'
+    end
+    
+    it "Returns 402 error status" do
+      unsuccessful_token_2
+      expect(response.status).to eq 402
     end
   end
 end
