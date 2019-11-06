645194387cd9d2337a48c962599cc27da59ca553
AlexSaxena <saxenaalex@gmail.com>
2019-11-06 17:40:22 +0100

    Adds Subsriber role to Seed file; Adds Categories to seed file, Adds Articles to seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,64 @@
 users = User.create(email: 'user@mail.com', password: 'password')
 journalist = User.create(email: 'journalist@mail.com', password: 'password', role: 'journalist')
+subscriber = User.create(email: 'subscriber@mail.com', password: 'password', role: 'subscriber')
+
+category_1 = Category.create( name: 'Sports')
+category_2 = Category.create( name: 'Politics')
+category_3 = Category.create( name: 'Tech')
+category_4 = Category.create( name: 'Economics')
+category_5 = Category.create( name: 'Lifestyle')
+category_6 = Category.create( name: 'Leisure')
+
+2.times do
+  Article.create(
+    title: Faker::Book.title
+    content: Faker::Movies::HitchhikersGuideToTheGalaxy.quote
+    journalist_id: rand(100)
+    category: category_1
+  )
+end
+
+2.times do
+  Article.create(
+    title: Faker::Book.title
+    content: Faker::Movies::HitchhikersGuideToTheGalaxy.quote
+    journalist_id: rand(100)
+    category: category_2
+  )
+end
+
+2.times do
+  Article.create(
+    title: Faker::Book.title
+    content: Faker::Movies::HitchhikersGuideToTheGalaxy.quote
+    journalist_id: rand(100)
+    category: category_3
+  )
+end
+
+2.times do
+  Article.create(
+    title: Faker::Book.title
+    content: Faker::Movies::HitchhikersGuideToTheGalaxy.quote
+    journalist_id: rand(100)
+    category: category_4
+  )
+end
+
+2.times do
+  Article.create(
+    title: Faker::Book.title
+    content: Faker::Movies::HitchhikersGuideToTheGalaxy.quote
+    journalist_id: rand(100)
+    category: category_5
+  )
+end
+
+2.times do
+  Article.create(
+    title: Faker::Book.title
+    content: Faker::Movies::HitchhikersGuideToTheGalaxy.quote
+    journalist_id: rand(100)
+    category: category_6
+  )
+end
