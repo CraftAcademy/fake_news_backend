56f43d6bcdd9f4d65cb26136eb9ca1677b8a0e88
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-11-07 14:59:26 +0100

    Re-add categories and articles to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,70 @@
 users = User.create(email: 'user@mail.com', password: 'password')
 journalist = User.create(email: 'journalist@mail.com', password: 'password', role: 'journalist')
 subscriber = User.create(email: 'subscriber@mail.com', password: 'password', role: 'subscriber')
+
+category_1 = Category.create( name: 'Sports')
+category_2 = Category.create( name: 'Politics')
+category_3 = Category.create( name: 'Tech')
+category_4 = Category.create( name: 'Economics')
+category_5 = Category.create( name: 'Lifestyle')
+category_6 = Category.create( name: 'Leisure')
+
+2.times do
+  Article.create(
+    title: Faker::Book.title,
+    content: Faker::Movies::HitchhikersGuideToTheGalaxy.quote,
+    journalist_id: journalist.id,
+    category: category_1,
+    image: Faker::LoremFlickr.image
+  )
+end
+
+2.times do
+  Article.create(
+    title: Faker::Book.title,
+    content: Faker::Movies::HitchhikersGuideToTheGalaxy.quote,
+    journalist_id: journalist.id,
+    category: category_2,
+    image: Faker::LoremFlickr.image
+  )
+end
+
+2.times do
+  Article.create(
+    title: Faker::Book.title,
+    content: Faker::Movies::HitchhikersGuideToTheGalaxy.quote,
+    journalist_id: journalist.id,
+    category: category_3,
+    image: Faker::LoremFlickr.image,
+  )
+end
+
+2.times do
+  Article.create(
+    title: Faker::Book.title,
+    content: Faker::Movies::HitchhikersGuideToTheGalaxy.quote,
+    journalist_id: journalist.id,
+    category: category_4,
+    image: Faker::LoremFlickr.image
+  )
+end
+
+2.times do
+  Article.create(
+    title: Faker::Book.title,
+    content: Faker::Movies::HitchhikersGuideToTheGalaxy.quote,
+    journalist_id: journalist.id,
+    category: category_5,
+    image: Faker::LoremFlickr.image
+  )
+end
+
+2.times do
+  Article.create(
+    title: Faker::Book.title,
+    content: Faker::Movies::HitchhikersGuideToTheGalaxy.quote,
+    journalist_id: journalist.id,
+    category: category_6,
+    image: Faker::LoremFlickr.image
+  )
+end
